ad6691bdfcb5e5adbefb2474a57361b4ca3a9f27
Fatiha Bedar <bedar.fatiha@gmail.com>
2023-07-14 13:49:16 -0700

    final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-* main - 5-printf.c
-* 
-* Return: Always 0 (Success)
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
