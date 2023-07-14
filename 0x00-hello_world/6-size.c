20208190854389bd41813e2112cec681a912a877
Fatiha Bedar <bedar.fatiha@gmail.com>
2023-07-14 13:06:07 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-* main - Entry point 
-* 
+* main - 6-size.c 
+* description - Write a C program that prints the size of various types on
+* the computer it is compiled and run on.
 * Return: Always 0 (Success)
 *
 */
