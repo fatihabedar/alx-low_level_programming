d596e97a4fa2b8b20b418e7958ff5a6715d1189a
Fatiha Bedar <bedar.fatiha@gmail.com>
2023-07-13 18:40:38 -0700

    step 8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * mai- Entry point
+ * 
+ * return: Always (success
+ * /
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * mai- Entry point
+ * 
+ * return: Always (success
+ * /
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art");
+    return 0;
+}
